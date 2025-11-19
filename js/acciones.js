7231ff5e385f8e134e0adb6f2bb7402e92e92aa9
laurav-m <laura.villalba.machacon@gmail.com>
2025-11-19 11:17:18 +0100

    Agrego nuevo parrafo descriptivo a index.html

---
+++ js/acciones.js
@@ -0,0 +1,7 @@
+console.log("Bienvenido al portafolio de Laura Villalba");
+//con alert() aparezerá una ventana emergente con un mensaje
+alert("Gracias por visitar mi portafolio!");
+console.log("Página cargada correctamente");
+//con document.querySelector() se busca un elemento dentro del html para modificarlo
+document.querySelector("h1").style.color="blue";
+//cambiamos el color del titulo a azul
